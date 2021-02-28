697904998e75869aa2429922778b300943fd26b6
RageousFoxTrot <rageousfoxtrot@gmail.com>
2021-02-28 04:56:30 +0000

    Fixed wrong arguments

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
     let video = document.querySelector('video');
     
     if ('mediaDevices' in navigator) {
-        navigator.mediaDevices.getUserMedia({ audio: false })
+        navigator.mediaDevices.getUserMedia({ audio: false, video: { width: 720, height: 1080 } })
         .then(stream => {
             video.srcObject = stream;
             video.addEventListener('loadedmetadata', () => video.play());
