dd580f1a2104bdf8898d2334da8fc066e4d514df
RageousFoxTrot <rageousfoxtrot@gmail.com>
2021-02-28 05:06:19 +0000

    Edited constraints

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,26 @@ window.addEventListener('DOMContentLoaded', () => {
     let video = document.querySelector('video');
     
     if ('mediaDevices' in navigator) {
-        navigator.mediaDevices.getUserMedia({ audio: false, video: { width: 720, height: 1080 } })
+        const constraints = {
+            audio: false,
+            video: {
+                width: {
+                    min: 720,
+                    max: 1440,
+                    ideal: 1080
+                },
+                height: {
+                    min: 1280,
+                    max: 2560,
+                    ideal: 1920
+                },
+                facingMode: {
+                    exact: 'environment'
+                }
+            }
+        };
+
+        navigator.mediaDevices.getUserMedia(constraints)
         .then(stream => {
             video.srcObject = stream;
             video.addEventListener('loadedmetadata', () => video.play());
